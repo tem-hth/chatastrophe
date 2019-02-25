048ac428b204d26a2d28bfb0169c464e0caafd2d
tem-hth <tem@episof.com>
2019-02-24 21:48:54 -0500

    moved react code to proper files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-require('./index2.js');
-console.log('hello from index.js!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(React.createElement('h1', false, 'Hello from React!'), document.getElementById('root'));
