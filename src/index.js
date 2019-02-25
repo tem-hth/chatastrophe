582f6c314066041b4027445c3f526e26cb469e9c
tem-hth <tem@episof.com>
2019-02-24 22:31:22 -0500

    change imports to ES6 format

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
 
 ReactDOM.render(React.createElement('h1', false, 'Hello from Webpack!'), document.getElementById('root'));
