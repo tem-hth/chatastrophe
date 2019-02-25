a6e7dfbdb3dddcec53b7589d829d6f70feeda049
tem-hth <tem@episof.com>
2019-02-24 22:41:26 -0500

    seperate jsx into app.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-
-ReactDOM.render(<h1>Hello from ES6!</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
