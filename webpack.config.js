97c8e336846419c151b8399ef7197394a9f382f0
tem-hth <tem@episof.com>
2019-02-24 22:27:51 -0500

    updated babel version

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,21 @@ module.exports = {
         filename: "bundle.js",
         publicPath: "/"
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+
+            }
+        ]
+    },
     devServer: {
         contentBase: "./public",
         historyApiFallback: true,
