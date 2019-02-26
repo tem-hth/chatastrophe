5dd8309224772387fa944726162196588825bdc6
tem-hth <tem@episof.com>
2019-02-26 00:39:46 -0500

    change state example test

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 
 class LoginContainer extends Component {
+    state = {text: 'Hello from state!'}
+    handleClick = () => {
+        this.setState({text: 'State changed!'});
+    };
+
     render() {
         return (
             <div id="LoginContainer" className="inner-container">
                 <div id="Header">
                     <img src="/assets/icon.png" alt="logo" />
+                    <h1 onClick={this.handleClick}>{this.state.text}</h1>
                     <h1>Chatastrophe</h1>
                 </div>
                <form>
