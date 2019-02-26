5c088be6a2ea612287123a7cb69d59892925f8a1
tem-hth <tem@episof.com>
2019-02-26 00:56:06 -0500

    capture state test

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -2,22 +2,29 @@ import React, {Component} from 'react';
 
 class LoginContainer extends Component {
     state = {text: 'Hello from state!'}
-    handleClick = () => {
-        this.setState({text: 'State changed!'});
+    handleEmailChange = (event) => {
+        this.setState({ email: event.target.value});
     };
+
+    handlePasswordChange = (event) => {
+        this.setState({ password: event.target.value});
+    };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
 
     render() {
         return (
             <div id="LoginContainer" className="inner-container">
                 <div id="Header">
                     <img src="/assets/icon.png" alt="logo" />
-                    <h1 onClick={this.handleClick}>{this.state.text}</h1>
                     <h1>Chatastrophe</h1>
                 </div>
-               <form>
+               <form onSubmit={this.handleSubmit}>
                    <p>Sign in or sign up by entering your email and password</p>
-                   <input type="text" placeholder="Your email" />
-                   <input type="password" placeholder="Your password" />
+                   <input onChange={this.handleEmailChange} value={this.state.email} type="text" placeholder="Your email" />
+                   <input onChange={this.handlePasswordChange} value={this.state.password} type="password" placeholder="Your password" />
                    <button className="red light" type="submit">Login</button>
                </form>
             </div>
